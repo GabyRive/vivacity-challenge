824cdfc1c893b63758f1ddb95a2ed2eb876c1ea5
GabyRive <gabrielarivera@GabrielsMacBook.lan>
2017-07-15 19:59:38 -0400

    This is my first commit with a simple example of a map.

---
+++ index.js
@@ -0,0 +1,10 @@
+var express = require('express');
+
+var app = express();
+var port = process.env.PORT || 4000;
+
+app.use('/', express.static(__dirname + '/public'));
+
+app.listen(port, function(){
+  console.log('VivaCity challenge is listening on port 4000!');
+});
